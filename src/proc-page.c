7cda659f780f8eb48038246e01702a48202f97dd
floral-hearts <hermit183@outlook.com>
2025-03-05 21:52:34 +0900

    setDefaultProcPage関数を制作

--- src/proc-page.c
+++ src/proc-page.c
@@ -1,1 +1,11 @@
 #include "proc-page.h"
+
+ProcPage setDefaultProcPage(void) {
+    ProcPage ret;
+    ret.color.red = 0x00;
+    ret.color.green = 0x00;
+    ret.color.blue = 0x00;
+    ret.pos.x = 297f;
+    ret.pos.y = 210f;
+    return ret;
+}
