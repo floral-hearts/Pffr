e59b10a888deb4731d3ae6df018d04232c45cfeb
floral-hearts <hermit183@outlook.com>
2025-03-05 20:33:15 +0900

    ファイルのパスを設定
    main関数の引数を変更

--- src/main.c
+++ src/main.c
@@ -1,4 +1,46 @@
 #include "main.h"
 
-int main(void) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "pffr.h"
+
+int main(int argc, char *argv[]) {
+    Pffr pffr;
+    FILE *pf = NULL;
+
+ // ファイルのパスを設定
+    if(argc == 2) {
+        if(strlen(argv[1]) > 254) {
+            fputs("error: ファイル名が長すぎます\n", stderr);
+            exit(1);
+        }
+        strcpy(pffr.path, argv[1]);
+    } else if(argc < 2) {
+        int canSetPath = 0;
+        for(int i = 0; i < 256; i ++) {
+            sprintf(pffr.path, "名称未設定%d.pffr", i);
+            pf = fopen(pffr.path, "r");
+            if(!pf) {
+                fclose(pf);
+                canSetPath = 1;
+                break;
+            }
+        }
+        if(!canSetPath) {
+            fputs("error: 名称未設定のファイルが多すぎます\n", stderr);
+            exit(1);
+        }
+    } else {
+        fputs("error: 不要な引数があります\n", stderr);
+        exit(1);
+    }
+    pf = fopen(pffr.path, "r");
+    if(pf) {
+    } else {
+        createDefaultFile(pffr);
+    }
+    fclose(pf);
+
+    return 0;
 }
