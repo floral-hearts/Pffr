93d436d1bef29a90b75ee96a5c0e25ba685bc82b
floral-hearts <hermit183@outlook.com>
2025-03-05 20:05:54 +0900

    createDefaultFile関数を制作

--- src/pffr.c
+++ src/pffr.c
@@ -1,1 +1,20 @@
 #include "pffr.h"
+
+#include <stdio.h>
+
+void createDefaultFile(Pffr pffr) {
+    FILE *pf = fopen(pffr.path, "w");
+    fputs("doc Pffr 1\n", pf);
+    fputs("\n", pf);
+    fputs("\n", pf);
+    fputs("acs pgt 26\n", pf);
+    fputs("\n", pf);
+    fputs("\n", pf);
+    fputs("pgc 1\n", pf);
+    fputs("acs pgn 1 41\n", pf);
+    fputs("\n", pf);
+    fputs("\n", pf);
+    fputs("pgs 210 297\n", pf);
+    fputs("obj 0\n", pf);
+    fclose(pf);
+}
