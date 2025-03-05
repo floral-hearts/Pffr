791a3a1ac09c393d6bc293b97321df2124d6c1f6
floral-hearts <hermit183@outlook.com>
2025-03-06 08:48:12 +0900

    setDefaultObjRectangle関数を制作

--- src/obj-rectangle.c
+++ src/obj-rectangle.c
@@ -1,1 +1,10 @@
 #include "obj-rectangle.h"
+
+ObjRectangle setDefaultObjRectangle(void) {
+    ObjRectangle ret;
+    ret.start.x = 0f;
+    ret.start.y = 0f;
+    ret.end.x = 0f;
+    ret.end.y = 0f;
+    return ret;
+}
