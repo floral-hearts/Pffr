3ce5e943d1c6fffacd9ca18d911e27be7dae25b5
floral-hearts <hermit183@outlook.com>
2025-03-05 20:46:08 +0900

    setDefaultProcInfo関数を制作

--- src/proc-info.c
+++ src/proc-info.c
@@ -1,1 +1,10 @@
 #include "proc-info.h"
+
+#include <string.h>
+
+ProcInfo setDefaultProcInfo(void) {
+    ProcInfo ret;
+    strcpy(ret.title, "default title");
+    strcpy(ret.author, "default author");
+    return ret;
+}
