1bf4a0d65ae092622d9148771a2fd73bf3072e75
floral-hearts <hermit183@outlook.com>
2025-03-05 21:53:53 +0900

    pageメンバを挿入
    Pffr構造体に

--- src/pffr.h
+++ src/pffr.h
@@ -2,10 +2,12 @@
 #define PFFR_H
 
 #include "proc-info.h"
+#include "proc-page.h"
 
 typedef struct tagPffr {
     char path[255];
     ProcInfo info;
+    ProcPage *page;
 } Pffr;
 
 void createDefaultFile(Pffr pffr);
