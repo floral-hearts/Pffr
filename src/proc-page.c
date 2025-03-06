9b383d46a76ea6f0352e94f30f1ad0b6084a8428
floral-hearts <hermit183@outlook.com>
2025-03-06 13:35:13 +0900

    バグ修正

--- src/proc-page.c
+++ src/proc-page.c
@@ -1,12 +1,14 @@
 #include "proc-page.h"
 
+#include <stdlib.h>
+
 ProcPage setDefaultProcPage(void) {
     ProcPage ret;
-    ret.color.red = 0x00;
-    ret.color.green = 0x00;
-    ret.color.blue = 0x00;
-    ret.pos.x = 297f;
-    ret.pos.y = 210f;
+    ret.background.red = 0x00;
+    ret.background.green = 0x00;
+    ret.background.blue = 0x00;
+    ret.size.x = 297.f;
+    ret.size.y = 210.f;
     ret.obj = NULL;
     ret.objSize = 0;
     return ret;
