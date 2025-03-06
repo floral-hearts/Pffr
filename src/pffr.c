a0d51a01b7168ea4db34ef15e9893ad3096d6e59
floral-hearts <hermit183@outlook.com>
2025-03-06 12:59:45 +0900

    アクセスプロセス完成

--- src/pffr.c
+++ src/pffr.c
@@ -29,9 +29,10 @@ void createDefaultFile(Pffr *pffr) {
 void getFileContent(Pffr *pffr) {
     FILE *pf = fopen(pffr->path, "r");
     char token[255];
-    char c;
-    long puta;
-    long b;
+    int c;
+    long byte;
+    long acsInfo = -1;
+    long acsPage = -1;
 
  // マジックナンバー
     char magicNumber[13] = "doc Pffr 1\n\n\n";
@@ -43,14 +44,24 @@ void getFileContent(Pffr *pffr) {
         }
     }
 
- // アクセスプロセス
+ // アクセスプロセ
+    while(1) {
     char acsToken[4] = "acs ";
+    int next = 1;
     for(int i = 0; i < 4; i ++) {
         c = fgetc(pf);
+        if(i == 1 && next && c == '\n') {
+            break;
+        } else if(c != '\n') {
+            next = 1;
+        }
         if(c != acsToken[4]) {
             fputs("error: アクセスプロセスが破損しています\n", stderr);
             exit(1);
         }
+    }
+    if(next) {
+        break;
     }
     for(int i = 0;  i < 3; i ++) {
         c = fgetc(pf);
@@ -59,6 +70,44 @@ void getFileContent(Pffr *pffr) {
             exit(1);
         }
         token[i] = c;
+    }
+    token[3] = '\0';
+    if(fgetc(pf) != ' ') {
+        fputs("error: アクセスプロセスが破損しています\n", stderr);
+        exit(1);
+    }
+    if(!(strcmp(token, "inf") == 0 || strcmp(token, "pag") == 0)) {
+        fputs("error: アクセスプロセスが破損しています\n", stderr);
+        exit(1);
+    }
+    char acsStr[255] = { 0 };
+    long acs;
+    for(int i = 0; i < 255; i ++) {
+        c = fgetc(pf);
+        if('0' <= c || c <= '9') {
+            acsStr[i] = (char)c;
+        } if(c == '\n') {
+            break;
+        } else {
+            fputs("error: アクセスプロセスが破損しています\n", stderr);
+            exit(1);
+        }
+    }
+    for(int i = 0; i < strlen(acsStr); i --) {
+        long digit = 1;
+        for(int j = 0; j < i; j ++) {
+            digit *= 10;
+        }
+        acs += (digit * (acsStr[i] - '0'));
+    }
+    long *acsProc;
+    acsProc = strcmp(token, "inf") == 0 ? &acsInfo : &acsPage;
+    if(*acsProc == -1) {
+        fputs("error: アクセスプロセスが破損しています\n", stderr);
+        exit(1);
+    } else {
+        acsProc = acs;
+    }
     }
 
     fclose(pf);
