1f9ece3b5b80ecc51e3e4f7f114918936aa1f01a
floral-hearts <hermit183@outlook.com>
2025-03-06 09:22:56 +0900

    obj動的メモリwo

--- src/proc-page.h
+++ src/proc-page.h
@@ -2,11 +2,14 @@
 #define PROC__PAGE_H
 
 #include "color.h"
+#include "obj.h"
 #include "pos.h"
 
 typedef struct tagProcPage {
     Color background;
     Pos size;
+    Obj *obj;
+    int objSize;
 } ProcPage;
 
 ProcPage setDefaultProcPage(void);
