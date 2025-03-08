b3d52dba426608ab45758abe2362eed02c5d3f3b
floral-hearts <hermit183@outlook.com>
2025-03-08 21:15:53 +0900

    関数分割のバグ修正

--- src/pffr.c
+++ src/pffr.c
@@ -29,21 +29,19 @@ void createDefaultFile(Pffr *pffr) {
 
 void getFileContent(Pffr *pffr) {
     FILE *pf = fopen(pffr->path, "r");
-    char ope[4];
-    char str255[255];
-    int c;
-    long byte;
-    long acsInfo = -1;
-    long acsPage = -1;
-
+    pffr.acsInfo = -1;
+    pffr.acsPage = -1;
     magic(pffr, pf);
     access(pffr, pf);
     information(pffr, pf);
     page(pffr, pf);
+    fclose(pf);
 }
 
-void magic(Pffr *pffr, FILE pf) {
- // マジックナンバー
+void magic(Pffr *pffr, FILE *pf) {
+    char ope[4];
+    char str255[255];
+
     ope[0] = '\0';
     fgetToken(pf, ope, 4);
     if(strcmp(ope, "doc") != 0) {
@@ -57,180 +55,87 @@ void magic(Pffr *pffr, FILE pf) {
     }
     fgetToken(pf, str255, 255);
     if((pffr.version = atoi(str255)) != 1) {
-        fputs("error: cannnot open file\n", pf);
+        fputs("error: cannnot open file\n", stderr);
         exit(1);
     }
-    while(strcmp(str255, "\n\n\n") == 0) {
-        fgetToken(pf, str255, 255);
+    fgetToken(pf, str255, 255);
+    if(strcmp(str255, "\n\n\n") != 0) {
+        fputs("error: no find next process\n", stderr);
+        exit(1);
     }
 }
 
 void access(Pffr *pffr, FILE pf) {
- // アクセスプロセス
+    char ope[4];
+    char key[4];
+    char str255[255];
+
+    ope[1] = 0;
     while(1) {
-        char acsToken[4] = "acs ";
-        int next = 1;
-        for(int i = 0; i < 4; i ++) {
-            c = fgetc(pf);
-            if(i == 1 && next && c == '\n') {
-                break;
-            } else if(i == 0 && c == '\n') {
-                next = 1;
-            } else if(c != acsToken[i]) {
-                next = 0;
-                fputs("error: acs proc: acs演算子\n", stderr);
-                exit(1);
-            } else {
-                next = 0;
-            }
-        }
-        if(next) {
+        fgetToken(pf, ope, 4);
+        if(strcmp(ope, "acs") != 0) {
+            fputs("error: break operator\n", stderr);
+            exit(1);
+        } else if(strcmp(ope, "\n\n\n") != 0) {
             break;
         }
-        for(int i = 0;  i < 3; i ++) {
-            c = fgetc(pf);
-            if(c == EOF) {
-                fputs("error: acs proc: acs第1被演算子がありません\n", stderr);
-                exit(1);
-            }
-            token[i] = c;
-        }
-        token[3] = '\0';
-        if(fgetc(pf) != ' ') {
-            fputs("error: アクセスプロセスが破損しています\n", stderr);
-            exit(1);
-        }
-        if(!(strcmp(token, "inf") == 0 || strcmp(token, "pag") == 0)) {
-            fputs("error: acs proc: acs第1被演算子\n", stderr);
-            exit(1);
-        }
-        char acsStr[255] = { 0 };
-        long acs;
-        for(int i = 0; i < 255; i ++) {
-            c = fgetc(pf);
-            if('0' <= c && c <= '9') {
-                acsStr[i] = (char)c;
-            } else if(c == '\n') {
-                break;
-            } else {
-                fputs("error: acs proc: acs第2被演算子\n", stderr);
-                exit(1);
-            }
-        }
-        acs = atol(acsStr);
-        long *acsProc;
-        acsProc = strcmp(token, "inf") == 0 ? &acsInfo : &acsPage;
-        if(*acsProc != -1) {
-            fputs("error: acs proc: アクセス項目が重複しています\n", stderr);
-            exit(1);
+        fgetToken(pf, key, 4);
+        fgetToken(pf, str255, 255);
+        if(strcmp(key, "inf") == 0) {
+            pffr.acsInfo = atol(str255);
+        } else if(strcmp(key, "pag") == 0) {
+            pffr.acsPage = atol(str255);
         } else {
-            *acsProc = acs;
+            fputs("error: break 1st acs operand\n", stderr);
         }
     }
 }
 
 void information(Pffr *pffr, FILE *pf) {
- // インフォメーションプロセス
+    char ope[4];
+    char str255[255];
+
     pffr.info = setDefaultProcInfo();
-    if(acsInfo != -1) {
-        if(fseek(pf, acsInfo, SEEK_SET) != 0) {
-            fputs("error: acs proc: infプロセスのアクセス先が存在しません\n", stderr);
+    while(pffr.acsInfo != -1) {
+        if(fseek(pf, pffr.acsInfo, SEEK_SET) != 0) {
+            fputs("error: no find information process\n", stderr);
             exit(1);
         }
-        for(int i = 0; i < 3; i ++) {
-            c = fgetc();
-            if(c == EOF) {
-                fputs("error: inf proc: 演算子が壊れています\n", stderr);
-                exit(1);
-            }
-            ope[i] = c;
-        }
-        if(fgetc() != ' ') {
-            fputs("error: inf proc: 演算子が壊れています\n", stderr);
-            exit(1);
-        }
-        ope[3] = '\0';
+        fgetToken(pf, ope, 4);
         if(strcmp(ope, "ttl") == 0) {
-            int i = 0;
-            while((c = fgetc()) == '\n') {
-                if(c == EOF) {
-                    fputs("error: inf proc: 被演算子が途中で終わっています\n", stderr);
-                    exit(1);
-                }
-                pffr.info.title[i] = c;
-                i ++;
-            }
-            pffr.info.title[i] = '\0';
-        }
-        if(strcmp(ope, "ttl") == 0) {
-            int i = 0;
-            while((c = fgetc()) == '\n') {
-                if(c == EOF) {
-                    fputs("error: inf proc: 被演算子が途中で終わっています\n", stderr);
-                    exit(1);
-                }
-                pffr.info.title[i] = c;
-                i ++;
-            }
-            pffr.info.title[i] = '\0';
+            fgetToken(pf, str255, 255);
+            strcpy(pffr.info.title, str255);
         } else if(strcmp(ope, "ath") == 0) {
-            int i = 0;
-            while((c = fgetc()) == '\n') {
-                if(c == EOF) {
-                    fputs("error: inf proc: 被演算子が途中で終わっています\n", stderr);
-                    exit(1);
-                }
-                pffr.info.author[i] = c;
-                i ++;
-            }
-            pffr.info.author[i] = '\0';
+            fgetToken(pf, str255, 255);
+            strcpy(pffr.info.author, str255);
+        } else if(strcmp(ope, "\n\n\n") == 0) {
+            break;
+        } else {
+            fputs("error: break operator\n", stderr);
         }
     }
 }
 
 void page(Pffr *pffr, FILE *pf) {
- // ページプロセス
-    if(acsPage == -1) {
-        fputs("error: acs proc: ページプロセスが指定されていません\n", stderr);
-        exit(1);
-    } else if(fseek(pf, acsPage, SEEK_SET) != 0) {
-        fputs("error: acs proc: 指定されたページプロセスは存在しません\n", stderr);
-        exit(1);
-    }
-    for(int i = 0; i < 3; i ++) {
-        c = fgetc();
-        if(c == EOF) {
-            fputs("error: page proc: 演算子が壊れています\n", stderr);
-            exit(1);
-        }
-        ope[i] = c;
-    }
-    if(strcmp(ope, "tnp") != 0 || fgetc() != ' ') {
-        fputs("error: page proc: 演算子が間違っています\n", stderr);
+    char ope[4];
+    char str255[255];
+
+    if(acsPage == -1 || fseek(pf, acsPage, SEEK_SET) != 0) {
+        fputs("error: no find page process\n", stderr);
         exit(1);
     }
-    char tnpStr[255];
-    for(int i = 0; i < 254; i ++) {
-        c = fgetc();
-        if(c == EOF) {
-            fputs("error: page proc: tnp被演算子が間違っています\n", stderr);
-            exit(1);
-        } else if(c == '\n') {
-            tnpStr[i] = '\0';
-            break;
-        } else {
-            tnpStr[i] = (char)c;
-        }
+    fgetToken(pf, ope, 4);
+    if(strcmp(ope, "tpg") != 0) {
+        fputs("error: break operator\n", stderr);
+        exit(1);
     }
-    pffr->pageSize = atoi(tnpStr);
+    fgetToken(pf, str255, 255);
+    pffr->pageSize = atoi(str255);
     if(pffr->pageSize < 1) {
-        fputs("error: page proc: tnp被演算子が数字ではありません\n", stderr);
+        fputs("error: break 1st tnp operand\n", stderr);
         exit(1);
     }
     pffr->page = (ProcPage *)malloc(sizeof(ProcPage) * pffr->pageSize);
-
-    fclose(pf);
-    return;
 }
 
 void fgetToken(FILE *f, char *str, int strSize) {
