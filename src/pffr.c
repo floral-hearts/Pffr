701bd5aeb28c4e511889f964df1cbbf97b42da13
floral-hearts <hermit183@outlook.com>
2025-03-09 17:15:53 +0900

    kid関数
    obj関連以外

--- src/pffr.c
+++ src/pffr.c
@@ -36,6 +36,7 @@ void getFileContent(Pffr *pffr) {
     access(pffr, pf);
     information(pffr, pf);
     page(pffr, pf);
+    kid(pffr, pf);
     fclose(pf);
 }
 
@@ -154,6 +155,77 @@ void page(Pffr *pffr, FILE *pf) {
         }
         fgetToken(pf, str255, 255);
         pffr->page[num].acs = atol(str255);
+    }
+}
+
+void kid(Pffr *pffr, FILE *pf) {
+    char ope[4];
+    char key[4];
+    char str255[255];
+
+    int nextPage;
+    int setedClr;
+    int setedSiz;
+    int setedTob;
+
+    for(int i = 0; i < pffr.pageSize; i ++) {
+        nextPage = setedClr = setedSiz = setedTob = 0;
+        fseek(pf, pffr.page[i].acs, SEEK_SET);
+        pffr.page[i] = setDefaultProcPage();
+        while(1) {
+            fgetToken(pf, ope, 4);
+            if(strcmp(ope, "clr") == 0) {
+                if(setedClr) {
+                    error(pffr, "error: clr operator is duplicated\n");
+                } else {
+                    setedClr = 1;
+                }
+                fgetToken(pf, str255, 255);
+                pffr.page[i].background.red = atoi(str255);
+                fgetToken(pf, str255, 255);
+                pffr.page[i].background.green = atoi(str255);
+                fgetToken(pf, str255, 255);
+                pffr.page[i].background.blue = atoi(str255);
+                fgetToken(pf, key, 255);
+            } else if(strcmp(ope, "siz") == 0) {
+                if(setedSiz) {
+                    error(pffr, "error: clr operator is duplicated\n");
+                } else {
+                    setedSiz = 1;
+                }
+                fgetToken(pf, str255, 255);
+                pffr.page[i].size.x = atoi(str255);
+                fgetToken(pf, str255, 255);
+                pffr.page[i].size.y = atoi(str255);
+            } else if(strcmp(ope, "tob") == 0) {
+                if(setedTob) {
+                    error(pffr, "error: tob operator is duplicated\n");
+                } else {
+                    setedTob = 1;
+                }
+                fgetToken(pf, str255, 255);
+                pffr.page[i].objSize = atoi(str255);
+            } else {
+                error(pffr, "error: break operator\n");
+            }
+            fgetToken(pf, key, 4);
+            if(strcmp(key, "\n") == 0) {
+            } else if(strcmp(key, "\n\n") == 0) {
+                break;
+            } else if(strcmp(key, "\n\n") == 0) {
+                if(pffr.page[i].objSize == 0) {
+                    nextPage = 1;
+                } else if(pffr.page[i].objSize == 0) {
+                    error(pffr, "error: no find object\n");
+                }
+            }
+            if(pffr.page[i].objSize == 0) {
+                error(pffr, "error: no find object\n");
+            }
+        }
+        if(nextPage) {
+            continue;
+        }
     }
 }
 
